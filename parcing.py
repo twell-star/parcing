5bb2e8ba687e2c19d196a3fa844c5652025422bb
twell-star <novikov.igor@mail.ru>
2021-03-02 10:22:46 +0300

    Starters

---
+++ parcing.py
@@ -0,0 +1,19 @@
+#
+# Решение задачи Парсинг данных
+#
+
+# Это наш "слон". Мы будем его есть по частям.
+log = """name:Иван;gender:m;item:Часы;item_cost:9800
+name:Иван;gender:m;item:Фитнес-браслет;item_cost:12300
+name:Иван;gender:m;item:Кофемашина;item_cost:23500
+name:Петр;gender:m;item:Часы;item_cost:9800
+name:Петр;gender:m;item:Фитнес-браслет;item_cost:12300
+name:Петр;gender:m;item:Айфон;item_cost:77900
+name:Петр;gender:m;item:Чехол для телефона;item_cost:350
+name:Петр;gender:m;item:Кофемашина;item_cost:23500
+name:Дарья;gender:m;item:Айфон;item_cost:77900
+name:Марья;gender:m;item:Кофемашина;item_cost:23500
+name:Юлия;gender:m;item:Фитнес-браслет;item_cost:12300"""
+
+records_log = log.split('\n')
+print(records_log)
